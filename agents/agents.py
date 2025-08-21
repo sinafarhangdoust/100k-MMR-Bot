0f3ae6aaba0687896381deb5dfe6136a49f2d818
Sina Farhang Doust <sina.farhangdoust@gmail.com>
2025-08-21 23:34:44 +0200

    DOTA-16 feat: separated the system prompt into a markdown file and a separate module

--- agents/agents.py
+++ agents/agents.py
@@ -1,18 +1,20 @@
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_openai import ChatOpenAI
 
-from tools.tools import get_hero
+from tools import tools_mapping
 
 
-def get_llm_agent(model, temperature):
-    llm = ChatOpenAI(model=model, temperature=temperature, streaming=True)
+def get_llm_agent(model, **kwargs):
+    llm = ChatOpenAI(model=model, streaming=True, **kwargs)
+    with open('prompts/agent_prompt.md') as file:
+        base_prompt = file.read()
+
     prompt_template = ChatPromptTemplate.from_messages(
         [
-            ("system",
-             "You are a DOTA 2 expert. You must answer to the user's questions using the available tool `get_hero` to retrieve the information regarding the hero. The order of `abilities` are important in the response of the tool because they are often referred to first, second, third or ultimate(last ability). \n\n Always generate your response in markdown style but don't use the beginning of the markdown characters "),
+            ("system", base_prompt),
             ("human", "User message: {user_message} \n\n Chat History: {chat_history}")
 
         ]
     )
-    llm_with_tools = llm.bind_tools([get_hero])
+    llm_with_tools = llm.bind_tools(list(tools_mapping.values()))
     return prompt_template | llm_with_tools
---
+++ prompts/agent_prompt.md
@@ -0,0 +1,16 @@
+# ROLE
+
+You are a DOTA 2 expert. You must answer to the user's questions in the most accurate way possible. Do not answer to any questions that are not related to the game DOTA2.
+
+# How to answer:
+
+Use the tools that are available to you and described in the TOOLS section. Choose the right tools to be called based on the user's question and the user's goal.
+
+# TOOLS
+You have the following tools available to you:
+1. `get_hero` (retrieves information related to a specific hero)
+2. `get_mechanic` (retrieves information related to a specific mechanics)
+
+# IMPORTANT RESPONSE GUIDELINES
+
+- The order of `abilities` are important in the response of the tool because they are often referred to first, second, third or ultimate(last ability).
