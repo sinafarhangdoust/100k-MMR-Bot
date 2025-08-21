892f30402ca44fac77e1b7c7f7a4abb4f50b3635
Sina Farhang Doust <sina.farhangdoust@gmail.com>
2025-08-21 23:33:42 +0200

    DOTA-16 feat: changed the  to  to include heroes, mechanics and items

--- tools/dota_db.py
+++ tools/dota_db.py
@@ -1,12 +1,12 @@
-import glob
 import json
 from threading import Lock
+from pathlib import Path
 
 from utils import instantiate_s3_client
 from config import S3_BUCKET_NAME
 
-
-class HeroDB:
+# TODO: change the logic to lazy loading
+class DotaDB:
     _instance = None
     _lock = Lock()
 
@@ -17,6 +17,27 @@ class HeroDB:
                     cls._instance = super().__new__(cls)
         return cls._instance
 
+    def load_rag_storage(self):
+
+        for obj in self.s3_client.list_objects(Bucket=S3_BUCKET_NAME).get('Contents', []):
+            obj_path = Path(obj["Key"])
+            loaded_obj = self.s3_client.get_object(Bucket=S3_BUCKET_NAME, Key=obj["Key"])["Body"].read()
+
+            if obj_path.suffix == ".json":
+                content = json.loads(loaded_obj.decode("utf-8"))
+            else:
+                content = loaded_obj.decode("utf-8", errors="ignore")
+
+            if str(obj_path).startswith('heroes'):
+                self.heroes[obj_path.stem] = content
+            elif str(obj_path).startswith('items'):
+                parent_dir = obj_path.parent.stem
+                if parent_dir not in self.items:
+                    self.items[parent_dir] = {}
+                self.items[parent_dir][obj_path.stem] = content
+            elif str(obj_path).startswith('mechanics'):
+                self.mechanics[obj_path.stem] = content
+
     def __init__(self):
         # Prevent reinitialization on subsequent calls
         if hasattr(self, "_initialized") and self._initialized:
@@ -25,10 +46,7 @@ class HeroDB:
         self.s3_client = instantiate_s3_client()
 
         self.heroes = {}
-        items = self.s3_client.list_objects(Bucket=S3_BUCKET_NAME)
-        for file in glob.glob("heroes/*"):
-            with open(file) as json_file:
-                hero_data = json.load(json_file)
-            self.heroes[hero_data['name']] = hero_data
-
+        self.items = {}
+        self.mechanics = {}
+        self.load_rag_storage()
         self._initialized = True
