2b17bb91bbfe9c001c96fbbcd289fe2bae4bcada
Sina Farhang Doust <sina.farhangdoust@gmail.com>
2025-09-05 20:47:41 +0200

    DOTA-17 feat: implemented the hero item suggestion

--- advisors/item_build_advisor.py
+++ advisors/item_build_advisor.py
@@ -1,5 +1,6 @@
 import json
 import os
+from typing import Dict
 
 from opendota import OpenDota
 
@@ -32,8 +33,51 @@ class OpenDotaEnhanced(OpenDota):
 
         return json_data
 
+class ItemAdvisor:
 
-if __name__ == '__main__':
-    client = OpenDotaEnhanced(data_dir='./')
-    item_popularity = client.get_item_popularity(hero_id=102)
-    items_list  = client.get_items()
+    def __init__(self):
+        self.opendota_client = OpenDotaEnhanced(data_dir='./')
+        self.hero2id, self.id2hero = self._get_hero_to_id_mapping()
+        self.item2id, self.id2item = self._get_item_to_id_mapping()
+
+    def _get_hero_to_id_mapping(self):
+        heroes = self.opendota_client.get_heroes()
+        hero2id = {hero['localized_name']: hero['id'] for hero in heroes}
+        id2hero = {hero['id']: hero['localized_name'] for hero in heroes}
+
+        return hero2id, id2hero
+
+    def _get_item_to_id_mapping(self):
+        items = self.opendota_client.get_items()
+        item2id = {item['localized_name']: item['id'] for item in items}
+        id2item = {item['id']: item['localized_name'] for item in items}
+        return item2id, id2item
+
+    def get_hero_name(self, hero_id: int or str):
+        if isinstance(hero_id, str):
+            hero_id = int(hero_id)
+        return self.id2hero[hero_id]
+
+    def get_hero_id(self, hero_name: str):
+        return self.hero2id[hero_name]
+
+    def get_item_name(self, item_id: int or str):
+        if isinstance(item_id, str):
+            item_id = int(item_id)
+        return self.id2item[item_id]
+
+    def get_item_id(self, item_name: str):
+        return self.item2id[item_name]
+
+    def get_item_suggestion(self, hero_name: str) -> Dict:
+        hero_id = self.get_hero_id(hero_name)
+        raw_item_popularity = self.opendota_client.get_item_popularity(hero_id=hero_id)
+        item_popularity = {}
+        for stage, item_dict in raw_item_popularity.items():
+            item_popularity[stage] = []
+            for item_id, frequency in item_dict.items():
+                item_name = self.get_item_name(item_id)
+                item_popularity[stage].append((frequency, item_name))
+            item_popularity[stage].sort(key=lambda x: x[0], reverse=True)
+
+        return item_popularity
