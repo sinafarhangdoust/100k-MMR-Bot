c3067db43dcbf7d5b5d1de563d81be1d1baa5c52
Sina Farhang Doust <sina.farhangdoust@gmail.com>
2025-09-05 19:45:28 +0200

    DOTA-17 feat: added a new method to retrieve items list and their ids

--- advisors/item_build_advisor.py
+++ advisors/item_build_advisor.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 from opendota import OpenDota
 
 class OpenDotaEnhanced(OpenDota):
@@ -7,9 +10,30 @@ class OpenDotaEnhanced(OpenDota):
         filename = f"item_popularity_{hero_id}.json"
         return self.get(url, filename=filename, force=force)
 
+    def get_items(self, force: bool = False):
+        sql_query = "select * from items"
+        filename = f"items.json"
+
+        path = None
+        if filename is not None:
+            path = os.path.join(self.data_dir, filename)
+            if not force and os.path.isfile(path):
+                try:
+                    with open(path, "r", encoding="utf-8") as f:
+                        json_data = json.load(f)
+                    return json_data
+                except Exception:
+                    pass
+
+        json_data = self.explorer(sql_query)
+        if path is not None:
+            with open(path, "w", encoding='utf-8') as f:
+                json.dump(json_data, f, ensure_ascii=False)
+
+        return json_data
+
 
 if __name__ == '__main__':
     client = OpenDotaEnhanced(data_dir='./')
     item_popularity = client.get_item_popularity(hero_id=102)
-    heroes = client.get_heroes()
-    print()
+    items_list  = client.get_items()
